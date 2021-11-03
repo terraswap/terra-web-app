abcc4161b64470fe7bdb415dba67a9e99920c4d7
rockonterra <93266370+rockonterra@users.noreply.github.com>
2021-11-03 15:03:44 +0100

    Add ROCK token :)

--- src/helpers/token.ts
+++ src/helpers/token.ts
@@ -219,6 +219,7 @@ export const GetTokenSvg = (symbol: string) => {
         case "JUTA":
         case "DKWON":
         case "ORION":
+        case "ROCK":
           res = "/images/CW20/" + symbol + ".png"
           break
         default:
